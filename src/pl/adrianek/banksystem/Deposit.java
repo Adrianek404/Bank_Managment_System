bfbdc597cf26929e4ba6de3112d797ddd60c4f33
Adrianek404 <adr.rzeszutek@gmail.com>
2024-01-12 22:52:03 +0100

    1 update

---
+++ src/pl/adrianek/banksystem/Deposit.java
@@ -0,0 +1,2 @@
+package pl.adrianek.banksystem;public class Deposit {
+}
---
+++ src/pl/adrianek/banksystem/SignupTwo.java
@@ -0,0 +1,10 @@
+package pl.adrianek.banksystem;
+
+import javax.swing.*;
+
+public class SignupTwo extends JFrame {
+
+    public SignupTwo(){
+
+    }
+}
